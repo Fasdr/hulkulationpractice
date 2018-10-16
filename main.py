295c709db5b29033c794bcc909344df8cc1d3230
saa19 <saaa1997@gmail.com>
2018-10-16 22:52:11 +0700

    что-то сделал

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from math import exp
-"""
-Шаг первый создать массив значений функций на сетке
-u[x][t]
-"""
-h = 0.01 #шаг по x
-t = 0.01 #шаг по t
+
+"Задаем начальные параметры"
+
+h = 0.5  # шаг по x
+t = 0.5  # шаг по t
 a = 0.022
 
-nx = int(1/h)
-nt = int(1/t)
+nx = int(1/h)+1
+nt = int(1/t)+1
 
-u = np.zeros((nx, nt))
-f = np.zeros((nx, nt))
-
-for i in range(0, nx):
-    u[i][0] = -(i*h)**4+(i*h)**2
-
-for j in range(0, nt):
-    u[0][j] = (t*j)**2-(t*j)
-    u[nx-1][j] = (t*j)+(t*j)**2-t*exp(1)
+u = np.zeros((nt, nx))  # решение разностной схемы
+f = np.zeros((nt, nx))
+ru = np.zeros((nt, nx))  # точное решение уравнения
 
 for i in range(0, nx):
     for j in range(0, nt):
-        f[i][j] = (i*h)+2*(t*j)-exp(i*h)+a*(12*((i*h)**2)-2+t*exp(i*h))
+        ru[j][i] = -(i*h)**4+(i*h)**2+i*j*t*h+(j*t)**2-(t*j)*exp(i*h)
 
-plt.matshow(f)
+for i in range(0, nx):
+    u[0][i] = -(i*h)**4+(i*h)**2
+
+for j in range(0, nt):
+    u[j][0] = (t*j)**2-(t*j)
+    u[j][nx-1] = (t*j)+(t*j)**2-(t*j)*exp(1)
+
+for i in range(0, nx):
+    for j in range(0, nt):
+        f[j][i] = (i*h)+2*(t*j)-exp(i*h)+a*(12*((i*h)**2)-2+(t*j)*exp(i*h))
+
+"Решаем разностную схему"
+
+"""
+plt.matshow(u)
 plt.show()
-
+"""
