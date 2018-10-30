b100a4bf552e8c4cb621135eb313d97e2bb8b90e
saa19 <saaa1997@gmail.com>
2018-10-31 00:14:29 +0700

    что-то делал

--- main.py
+++ main.py
@@ -33,16 +33,16 @@ for i in range(0, nx):
 
 ll = np.zeros(nx-1)
 kk = np.zeros(nx-1)
-#
-# for j in range(1, nt):
-#     ll[0] = 0
-#     kk[0] = u[j][0]
-#     for i in range(1, nx-1):
-#         ll[i] = r/(1+2*r-r*ll[i-1])
-#         kk[i] = ((r*u[j-1][i-1]+(1-2*r)*u[j-1][i]+r*u[j-1][i+1]+t*f[j][i]/2+t*f[j-1][i]/2)+r*kk[i-1])/(1+2*r-r*ll[i-1])
-#     for i in range(nx-2, -1, -1):
-#         u[j][i] = ll[i]*u[j][i+1]+kk[i]
-#
+
+for j in range(1, nt):
+    ll[0] = 0
+    kk[0] = u[j][0]
+    for i in range(1, nx-1):
+        ll[i] = r/(1+2*r-r*ll[i-1])
+        kk[i] = ((r*u[j-1][i-1]+(1-2*r)*u[j-1][i]+r*u[j-1][i+1]+t*f[j][i]/2+t*f[j-1][i]/2)+r*kk[i-1])/(1+2*r-r*ll[i-1])
+    for i in range(nx-2, -1, -1):
+        u[j][i] = ll[i]*u[j][i+1]+kk[i]
+
 # b = u-ru
 # c = -b
 #
@@ -78,5 +78,3 @@ kk = np.zeros(nx-1)
 # plt.plot(tt, ru[:, int(0.5 / h)], 'r+')
 # plt.grid(True)
 # plt.show()
-
-
